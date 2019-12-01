7fc50d4369c781ce2507fc0c00c12923f8a61396
Courtney Davis <courtneydavis@The-Davis-iMac.local>
2019-12-01 18:37:07 -0500

    Add 404 page and combine home page to one component.

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>
+        Not Found <i class='far fa-frown'></i>
+      </h1>
+
+      <p className='lead'>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
